361d869b9cd75ac1975e1f866fb7d20643eaa3f5
Clare Curtis <cfcurti2@illinois.edu>
2014-03-18 00:21:14 -0500

    added some more stuff

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.heroku import Heroku
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
 import soundcloud
@@ -8,7 +9,9 @@ app.secret_key = 'a'
 app.debug = True
 
 # DB Stuff
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/mixt'
+heroku = Heroku(app)
+if not app.config['SQLALCHEMY_DATABASE_URI']:
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/mixt'
 db = SQLAlchemy(app)
 db.init_app(app)
 
