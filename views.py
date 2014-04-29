9f16d16f1751143a44824871650ce3443134e3a5
Clare Curtis <cfcurti2@illinois.edu>
2014-04-28 22:10:15 -0500

    dupe fix

--- views.py
+++ views.py
@@ -81,6 +81,9 @@ def add_track(url, uploader, source, songname, artist, genre, label, year):
 		print str(type(e)) + " " + str(e)
 
 def add_source(source, genre):
+	dup = db.session.query(Sources.subreddit).filter(Sources.subreddit==source).first()
+	if len(dupe) > 0:
+		return None
 	try:
 		src = Sources(source, genre)
 		db.session.add(src)
